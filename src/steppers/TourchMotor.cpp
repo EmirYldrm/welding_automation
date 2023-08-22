a66c52d9ec1ce0ca108eb59123128d69648ad550
emir yildirim <yildirimemir149@gmail.com>
2023-08-22 11:35:25 +0300

    tourch motor fonksiyonları eklendi

---
+++ src/steppers/TourchMotor.cpp
@@ -0,0 +1,50 @@
+#include "steppers/TourchMotor.h"
+#include "TourchMotor.h"
+
+AccelStepper tourchMotor(1, TOURCH_MOTOR_PUL, TOURCH_MOTOR_DIR);
+void init_limit_sensor()
+{
+    pinMode(LIMIT_SWITCH_PIN, INPUT_PULLUP);
+}
+
+void init_tourch_motor()
+{
+    tourchMotor.setMaxSpeed(200);
+    tourchMotor.setAcceleration(200);
+    tourchMotor.setSpeed(200);
+    tourchMotor.setCurrentPosition(0);
+
+    pinMode(TOURCH_MOTOR_ENA, OUTPUT);
+    // Motor enable edildi
+    digitalWrite(TOURCH_MOTOR_ENA, LOW);
+}
+
+void approachTourch()
+{
+    // Motor enable edildi
+    digitalWrite(TOURCH_MOTOR_ENA, LOW);
+    
+    delay(100);
+    while(digitalRead(LIMIT_SWITCH_PIN) == 0){
+      tourchMotor.setSpeed(100);  
+      tourchMotor.runSpeed();
+    }
+    tourchMotor.setCurrentPosition(0);
+    delay(100);
+}
+
+void retractTourch()
+{
+    // Motor enable edildi
+    digitalWrite(TOURCH_MOTOR_ENA, LOW);
+
+
+    tourchMotor.move(OPEN_POS_IN_STEPS);
+    tourchMotor.setMaxSpeed(200);
+    delay(100);
+    tourchMotor.runToPosition();
+    delay(100);
+    tourchMotor.setCurrentPosition(0);
+
+
+}
--- src/steppers/TourchMotor.h
+++ src/steppers/TourchMotor.h
@@ -7,8 +7,20 @@
 //E0 MOTORA BAXLANACAK
 #define TOURCH_MOTOR_PUL 26
 #define TOURCH_MOTOR_DIR 28
+#define TOURCH_MOTOR_ENA 24
 
-extern AccelStepper tourchMotor(1, TOURCH_MOTOR_PUL, TOURCH_MOTOR_DIR);
+#define LIMIT_SWITCH_PIN 14 // Y_MIN
+
+#define OPEN_POS_IN_STEPS 400   //farazi bir değer girildi denemelerde düzeltilecek
+
+extern AccelStepper tourchMotor;
+
+void init_limit_sensor();
+void init_tourch_motor();
+void approachTourch();
+void retractTourch();
+
+
 
 
 #endif
