1e2bad93f323d0893a01a79d079d2b19e742049d
emir yildirim <yildirimemir149@gmail.com>
2023-08-22 11:32:41 +0300

    init fonskiyonları eklendi

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 #include "servo/TourchTrigger.h"
 #include "button/StartButton.h"
 #include "steppers/SpinTable.h"
+#include "steppers/TourchMotor.h"
 
 void setup() {
+  
   Serial.begin(9600);
+
   init_servo_motor();
   init_start_button();
   init_spin_motor();
+  init_tourch_motor();
+  init_limit_sensor();
 
 }
 
