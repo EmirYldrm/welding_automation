b4b38d0a1beff200e9f9b050e941ff9006207e27
emir yildirim <yildirimemir149@gmail.com>
2023-08-24 18:40:47 +0300

    açıklama eklendi

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+/*
+                    EMİR YILDIRIM - AKİL ENDÜSTRİ 
+                      KAYNAK MAKİNESİ OTOMASYONU
+  Bu projede;
+  ->  Kaynatılacak parçayı döndürmek üzere bir adet NEMA 23 step motor,
+  ->  Kaynak makinesini yaklaştırmak için bir adet NEMA 17 step motor,
+  ->  Kaynak makinesinin min pozisyonunu bilebilmesi için mekanik limit switch,
+  ->  Kaynak makinesinin tetiğine basması için kullanılan MG SERVO motor,
+  ->  Kaynak işlemini başlatmak için bir adet buton (INPUT_PULLUP) 
+  ->  Kaynak işelemi yapılırken döner tablanın hızını ayarlamak için potansiyometre
+  kullanılmıştır.
+  (With contribution by M. Erol Gencay)
+ */
+
 #include <Arduino.h>
 
 
@@ -9,7 +23,7 @@
 void setup() {
   
   Serial.begin(9600);
-  
+
   init_EEPROM();
   init_servo_motor();
   init_start_button();
