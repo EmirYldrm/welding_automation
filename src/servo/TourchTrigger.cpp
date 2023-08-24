6924f5a00ec31340b721b394626bcaa20fef2bd8
emir yildirim <yildirimemir149@gmail.com>
2023-08-24 18:21:45 +0300

    servo motor initial pozisyonu düzenlendi

--- src/servo/TourchTrigger.cpp
+++ src/servo/TourchTrigger.cpp
@@ -6,7 +6,8 @@ Servo triggerServo;
 int pos = 0;
 
 void init_servo_motor()
-{
+{   
+    triggerServo.write(0);
     triggerServo.attach(SERVO_PIN);
 }
 
@@ -14,22 +15,23 @@ void openTourch()
 {
     Serial.println("TORC ACILDI");
 
-    for (pos = 0; pos <= 70; pos += 1) { // goes from 0 degrees to 180 degrees
+    /*for (pos = 0; pos <= 70; pos += 1) { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
         triggerServo.write(pos);              // tell servo to go to position in variable 'pos'
                           // waits 15 ms for the servo to reach the position
-    }
+    }*/
+    triggerServo.write(35);
     Serial.println("TORC ACILMASI BITTI ");
 }
 
 void closeTourch()
 {
     Serial.println("TORC KAPANDI");
-    int position = triggerServo.read();
-    Serial.println(position);
+    /*
     for (pos = 70; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
         triggerServo.write(pos);              // tell servo to go to position in variable 'pos'
                               // waits 15 ms for the servo to reach the position
     }
-    
+    */
+   triggerServo.write(0);
 }
