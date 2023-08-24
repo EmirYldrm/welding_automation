8fa8b7a6f7be68e4435f2acbd2de924d9a5f4185
emir yildirim <yildirimemir149@gmail.com>
2023-08-24 18:21:31 +0300

    eeprom ekelndi

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <Arduino.h>
+
+
 #include "servo/TourchTrigger.h"
 #include "button/StartButton.h"
 #include "steppers/SpinTable.h"
 #include "steppers/TourchMotor.h"
-
+#include "eeprom/eepromHandler.h"
 void setup() {
   
   Serial.begin(9600);
-
+  
+  init_EEPROM();
   init_servo_motor();
   init_start_button();
   init_spin_motor();
